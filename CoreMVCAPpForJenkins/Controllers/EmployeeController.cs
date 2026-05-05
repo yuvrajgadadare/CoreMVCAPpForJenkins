7f2c5e929b372f7499ba0b07e6520f54c18ba10c
yuvrajgadadare <yuvraj.gadadare@gmail.com>
2026-05-05 08:07:17 +0530

    Added Controller

---
+++ CoreMVCAPpForJenkins/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCAPpForJenkins.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View()
+        }
+    }
+
